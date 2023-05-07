97c1f47d06b11c889c8985650b17e5d95e0a6e33
Pedrowill21 <pedrowilliam2999@gmail.com>
2023-05-07 16:42:05 -0300

    eventos e estilização

--- Javascript/js.js
+++ Javascript/js.js
@@ -1,6 +1,7 @@
-let nome = prompt("Digite seu nome");
-let altura = prompt("Digite sua altura em Centímetros");
-let peso = prompt("Digite seu Peso em KG");
+  function calcImc(){
+    let nome = document.getElementById('nome').value;
+    let altura = document.getElementById('altura').value;
+    let peso = document.getElementById('peso').value;
 
 altura = parseFloat(altura);
 peso = parseFloat(peso);
@@ -11,25 +12,43 @@ let imc = peso / (altura * altura);
 
 imc = imc.toFixed(2);
 
-let classificacao = undefined ;
+let classificacao = '';
+let alerta = "";
 
 if(imc <= 16){
     classificacao = "Baixo peso muito grave"
-
+    alerta = "red"
 } else if( imc > 16 && imc < 16.99){
     classificacao = "Baixo peso grave"
+    alerta = "red"
 } else  if( imc > 17 && imc < 17.49){
     classificacao = "Baixo peso"
+    alerta = "orange"
 } else  if( imc > 18.50 && imc < 24.99){
     classificacao = "Peso Normal"
+    alerta = "blue"
 } else  if( imc > 25 && imc < 29.99){
     classificacao = "Sobrepeso"
+    alerta = "red"
 } else  if( imc > 30 && imc < 34.99){
     classificacao = "Obesidade Grau 1"
+    alerta = "red"
 } else  if( imc > 35 && imc < 39.99){
     classificacao = "Obesidade Grau 2"
+    alerta = "red"
 } else  if( imc > 40){
     classificacao = "Obesidade Grau 3"
+    alerta = "red"
 }
 
-document.write( nome + ", possui índice de massa corporal igual a : " + imc + ", sendo classificado como:" + classificacao )
+document.getElementById('saida').style.background = alerta;
+document.getElementById('saida').style.color = "white";
+ document.getElementById('saida').value = classificacao;
+ document.getElementById('nome').value = '';
+ document.getElementById('altura').value = '';
+ document.getElementById('peso').value = '';
+
+
+
+  }
+
